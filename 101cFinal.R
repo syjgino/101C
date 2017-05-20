3488d7fac040c651fcd14627332dbea8858bcff5
jerryfan0123 <jerryfan@ucla.edu>
2017-05-20 15:50:45 -0700

    Update 101cFinal.R

--- 101cFinal.R
+++ 101cFinal.R
@@ -1,6 +1,11 @@
 
 library(ggplot2)
+
+#working directory will depend on your computer
 setwd("C:/school/spring 2017/101C/project")
 
+#load data
 data_train <- read.csv("lafdtraining.csv")
-data_train <- na.omit(data_train)
+
+#data_train_clean will be the name for our cleaned data
+data_train_clean <- na.omit(data_train)
