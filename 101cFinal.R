2024ccf8dba334bec5e3a076c2764201514c7e7b
Gino Su <syjgino@gmail.com>
2017-05-26 18:35:02 -0700

    Update 101cFinal.R

--- 101cFinal.R
+++ 101cFinal.R
@@ -1,29 +1,34 @@
 library(ggplot2)
 library(chron)
 library(dplyr)
-setwd("C:/school/spring 2017/101C/project")
+#working directory will depend on your computer
+#setwd("C:/school/spring 2017/101C/project")
+#setwd("~/Desktop/UCLA/2017Spring/101c/101cFinal")
 
+#load data
 data_train <- read.csv("lafdtraining.csv")
-data_train <- na.omit(data_train)
+
+#data_train_clean will be the name for our cleaned data (all na removed)
+data_train_clean <- na.omit(data_train)
 
 #change creation time to hours since midnight
-data_train$Incident.Creation.Time..GMT. <- 24 * as.numeric(times(data_train$Incident.Creation.Time..GMT.))
+data_train_clean$Incident.Creation.Time..GMT. <- 24 * as.numeric(times(data_train_clean$Incident.Creation.Time..GMT.))
 
 #add new variable that groups creation times into 4 hour factors
-data_train <- mutate(data_train, time_group = as.factor(floor(data_train$Incident.Creation.Time..GMT./3))) #divide by whatever interval you want
+data_train_clean <- mutate(data_train_clean, time_group = as.factor(floor(data_train_clean$Incident.Creation.Time..GMT./.5))) #divide by whatever interval you want
 
 #rename variables
-colnames(data_train) <- c("row", "ID", "year", "district", "emergency_code", "dispatch_seq", "dispatch_type", "unit_type", "PPE_level", "creation_time", "y", "time_group")
+colnames(data_train_clean) <- c("row", "ID", "year", "district", "emergency_code", "dispatch_seq", "dispatch_type", "unit_type", "PPE_level", "creation_time", "y", "time_group")
 
 #change year, district, dispatch_seq to factor
-data_train$dispatch_seq <- as.character(data_train$dispatch_seq)
+data_train_clean$dispatch_seq <- as.character(data_train_clean$dispatch_seq)
 
-data_train$district <- as.character(data_train$district)
+data_train_clean$district <- as.character(data_train_clean$district)
 
-data_train$year <- as.character(data_train$year)
+data_train_clean$year <- as.character(data_train_clean$year)
 
 #delete row, ID, emergency_code, creation_time
-data_train_1 <- data_train[, -c(1,2,5,10)]
+data_train_1 <- data_train_clean[, -c(1,2,5)]
 
 #set seed to your ID
 set.seed(404318564)
@@ -37,3 +42,6 @@ data_subset <- data_train_1[x, ]
 m1 <- lm(data = data_subset, y ~ .)
 
 
+#plot creation time vs y
+ggplot()+
+    geom_boxplot(data = data_subset, aes(y,time_group))
