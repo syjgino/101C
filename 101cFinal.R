e1d0c9132658112a616e414c8759b769f1ca1f30
jerryfan0123 <jerryfan@ucla.edu>
2017-05-26 17:41:33 -0700

    Update 101cFinal.R

--- 101cFinal.R
+++ 101cFinal.R
@@ -10,6 +10,18 @@ data_train <- read.csv("lafdtraining.csv")
 data_train_clean <- na.omit(data_train)
 data_train_clean <- data_train[data_train_clean$elapsed_time > 0, ]
 
+#change creation time to hours since midnight
+data_train$Incident.Creation.Time..GMT. <- 24 * as.numeric(times(data_train$Incident.Creation.Time..GMT.))
+
+#add new variable that groups creation times into 4 hour factors
+data_train <- mutate(data_train, time_group = as.factor(floor(data_train$Incident.Creation.Time..GMT./4)))
+
+#set seed to your ID
+set.seed(404318564)
+
+#sample
+x <- sample(1:2315060, 50000)
+
 #distribution of elapsed_time
 hist(data_train_clean$elapsed_time)
 boxplot(data_train_clean$elapsed_time)
