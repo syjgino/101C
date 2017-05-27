66a9f341028ba09bfee6d933a4871d0adec85f1e
jerryfan0123 <jerryfan@ucla.edu>
2017-05-26 18:17:32 -0700

    Update 101cFinal.R

--- 101cFinal.R
+++ 101cFinal.R
@@ -10,19 +10,20 @@ data_train <- na.omit(data_train)
 data_train$Incident.Creation.Time..GMT. <- 24 * as.numeric(times(data_train$Incident.Creation.Time..GMT.))
 
 #add new variable that groups creation times into 4 hour factors
-data_train <- mutate(data_train, time_group = as.factor(floor(data_train$Incident.Creation.Time..GMT./4)))
+data_train <- mutate(data_train, time_group = as.factor(floor(data_train$Incident.Creation.Time..GMT./4))) #divide by whatever interval you want
 
 #rename variables
 colnames(data_train) <- c("row", "ID", "year", "district", "emergency_code", "dispatch_seq", "dispatch_type", "unit_type", "PPE_level", "creation_time", "y", "time_group")
 
-#delete emergency_code
-data_train <- data_train[, -5]
 #change year, district, dispatch_seq to factor
 data_train$dispatch_seq <- as.character(data_train$dispatch_seq)
 
 data_train$district <- as.character(data_train$district)
 
 data_train$year <- as.character(data_train$year)
+
+#delete row, ID, emergency_code, creation_time
+data_train_1 <- data_train[, -c(1,2,5,10)]
 
 #set seed to your ID
 set.seed(404318564)
@@ -31,8 +32,8 @@ set.seed(404318564)
 x <- sample(1:2315060, 50000)
 
 #create subset
-data_subset <- data_train[x, ]
+data_subset <- data_train_1[x, ]
 
-m1 <- lm(data = data_subset, y ~ . - row - ID - creation_time)
+m1 <- lm(data = data_subset, y ~ .)
 
 
