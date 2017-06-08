8c587c51d0f138a0c10d493a4bdef7313566d048
jerryfan0123 <jerryfan@ucla.edu>
2017-06-07 17:02:40 -0700

    Update 101cFinal.R

--- 101cFinal.R
+++ 101cFinal.R
@@ -5,30 +5,70 @@ library(dplyr)
 #setwd("C:/school/spring 2017/101C/project")
 #setwd("~/Desktop/UCLA/2017Spring/101c/101cFinal")
 
-#load data
-data_train <- read.csv("lafdtraining.csv")
+raw_data <- read.csv("lafdtraining.csv")
 
-#data_train_clean will be the name for our cleaned data (all na removed)
-data_train_clean <- na.omit(data_train)
+cleaned_data <- raw_data
 
 #change creation time to hours since midnight
-data_train_clean$Incident.Creation.Time..GMT. <- 24 * as.numeric(times(data_train_clean$Incident.Creation.Time..GMT.))
+cleaned_data$Incident.Creation.Time..GMT. <- 24 * as.numeric(times(cleaned_data$Incident.Creation.Time..GMT.))
 
 #add new variable that groups creation times into 4 hour factors
-data_train_clean <- mutate(data_train_clean, time_group = as.factor(floor(data_train_clean$Incident.Creation.Time..GMT./.5))) #divide by whatever interval you want
+cleaned_data <- mutate(cleaned_data, time_group = as.factor(floor(cleaned_data$Incident.Creation.Time..GMT.))) #divide by whatever interval you want
 
-#rename variables
-colnames(data_train_clean) <- c("row", "ID", "year", "district", "emergency_code", "dispatch_seq", "dispatch_type", "unit_type", "PPE_level", "creation_time", "y", "time_group")
+colnames(cleaned_data) <- c("row", "ID", "year", "district", "emergency_code", "dispatch_seq", "dispatch_type", "unit_type", "PPE_level", "creation_time", "y", "time_group")
 
-#change year, district, dispatch_seq to factor
-data_train_clean$dispatch_seq <- as.character(data_train_clean$dispatch_seq)
+#remove rows with NA response
+cleaned_data <- cleaned_data[!is.na(cleaned_data$y),]
 
-data_train_clean$district <- as.character(data_train_clean$district)
+#change ID, year, district, and dispatch_seq to factor
+cleaned_data$ID <- as.character(cleaned_data$ID)
+cleaned_data$ID <- as.factor(cleaned_data$ID)
 
-data_train_clean$year <- as.character(data_train_clean$year)
+cleaned_data$year <- as.character(cleaned_data$year)
+cleaned_data$year <- as.factor(cleaned_data$year)
 
-#delete row, ID, emergency_code, creation_time
-data_train_1 <- data_train_clean[, -c(1,2,5)]
+cleaned_data$district <- as.character(cleaned_data$district)
+
+cleaned_data$dispatch_seq <- as.character(cleaned_data$dispatch_seq)
+cleaned_data$dispatch_seq <- as.factor(cleaned_data$dispatch_seq)
+
+
+#delete row, emergency_code, creation_time
+cleaned_data <- cleaned_data[, -c(1, 5,10)]
+
+#use knnimpute to fill in missing na values
+library(VIM)
+#cleaned_data_impute <- kNN(data = cleaned_data, variable = c("dispatch_seq", "PPE_level"), k = 2)
+
+#cleaned_data_mean <- cleaned_data
+#cleaned_data_mean$dispatch_seq <- 
+
+#temp_data just omits all na values
+temp_data <- na.omit(cleaned_data)
+
+
+
+testing_data <- read.table("testing.without.response", sep= ",", header = T)
+
+#change creation time to hours since midnight
+testing_data$Incident.Creation.Time..GMT. <- 24 * as.numeric(times(testing_data$Incident.Creation.Time..GMT.))
+
+#add new variable that groups creation times into 4 hour factors
+testing_data <- mutate(testing_data, time_group = as.factor(floor(testing_data$Incident.Creation.Time..GMT.))) #divide by whatever interval you want
+
+colnames(testing_data) <- c("row", "ID", "year", "district", "emergency_code", "dispatch_seq", "dispatch_type", "unit_type", "PPE_level", "creation_time", "time_group")
+
+#change ID, year, district, and dispatch_seq to factor
+testing_data$ID <- as.character(testing_data$ID)
+testing_data$ID <- as.factor(testing_data$ID)
+
+testing_data$year <- as.character(testing_data$year)
+testing_data$year <- as.factor(testing_data$year)
+
+testing_data$district <- as.character(testing_data$district)
+
+testing_data$dispatch_seq <- as.character(testing_data$dispatch_seq)
+testing_data$dispatch_seq <- as.factor(testing_data$dispatch_seq)
 
 #set seed to your ID
 set.seed(404318564)
